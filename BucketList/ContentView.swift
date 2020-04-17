6997041cbefae9c501f8465af89ce81aa05486d8
misaellandero <misaellandero@icloud.com>
2020-04-17 16:23:24 -0500

    ahora se guardan los datos en los archivos de la pp

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -13,52 +13,66 @@ import MapKit
 struct ContentView: View {
   @State private var isUnlook = false
   @State private var centerCoordinate = CLLocationCoordinate2D()
-  @State private var locations = [MKPointAnnotation]()
+  @State private var locations = [CodableMKPointAnnotation]()
   @State private var selectedPlace: MKPointAnnotation?
   @State private var showingPlaceDetails = false
   @State private var showingEditaScreen = false
     var body: some View {
         ZStack {
-            MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations).edgesIgnoringSafeArea(.all)
-            Circle()
+            if isUnlook {
+                MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations).edgesIgnoringSafeArea(.all)
+                Circle()
                 .fill(Color.blue)
                 .opacity(0.3)
                 .frame(width: 32, height: 32)
-            VStack{
-                Spacer()
-                HStack{
-                        Spacer()
-                        Button(action: {
-                            let newLocation = MKPointAnnotation()
-                            newLocation.title = "Locación de ejemplo"
-                            newLocation.coordinate =  self.centerCoordinate
-                            self.locations.append(newLocation)
-                            
-                            self.selectedPlace = newLocation
-                            self.showingEditaScreen = true
-                        }){
-                            Image(systemName: "plus")
-                        }
-                    .padding()
-                        .background(Color.black.opacity(0.75))
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .clipShape(Circle())
-                        .padding(.trailing)
+                VStack{
+                    Spacer()
+                    HStack{
+                            Spacer()
+                            Button(action: {
+                                let newLocation = CodableMKPointAnnotation()
+                                newLocation.title = "Locación de ejemplo"
+                                newLocation.coordinate =  self.centerCoordinate
+                                self.locations.append(newLocation)
+                                
+                                self.selectedPlace = newLocation
+                                self.showingEditaScreen = true
+                            }){
+                                Image(systemName: "plus")
+                            }
+                        .padding()
+                            .background(Color.black.opacity(0.75))
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .clipShape(Circle())
+                            .padding(.trailing)
+                    }
                 }
+            } else {
+                Button("Unlock Places") {
+                    self.authenticate()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
             }
+            
+            
+            
         }
         .alert(isPresented: $showingPlaceDetails){
             Alert(title: Text(selectedPlace?.title ?? "Desconocido"), message: Text(selectedPlace?.subtitle ?? "Informacion del lugar desconocida"), primaryButton: .default(Text("Ok")), secondaryButton: .default(Text("Editar")){
                 self.showingEditaScreen = true
             })
         }
-        .sheet(isPresented: $showingEditaScreen){
+        .sheet(isPresented: $showingEditaScreen, onDismiss: SaveData){
             if self.selectedPlace != nil {
                 EditView(placemark: self.selectedPlace!)
             }
         }
-    .onAppear(perform: authenticate)
+        .onAppear(perform: LoadData /*,authenticate*/)
+        .onAppear(perform: authenticate)
     }
     
     func authenticate(){
@@ -83,6 +97,34 @@ struct ContentView: View {
         }
         
     }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func LoadData(){
+        let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+        
+       do{
+            let data = try Data(contentsOf: filename)
+            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
+        } catch {
+            print("Unable to load saved data.")
+        }
+    }
+    
+    func SaveData() {
+         do {
+               let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+               let data = try JSONEncoder().encode(self.locations)
+               try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+           } catch {
+               print("Unable to save data.")
+           }
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
